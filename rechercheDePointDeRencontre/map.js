13e0d9844d40a94d603c4f9caa830ab9b7ecc34a
casarin2012 <julien.casarin@gmail.com>
2014-12-02 11:18:37 +0100

    recherche points de rdv ok

--- rechercheDePointDeRencontre/map.js
+++ rechercheDePointDeRencontre/map.js
@@ -6,7 +6,7 @@ function Map(){
 	
 	
 	
-	this.loadMap = function( data ){
+	/*this.loadMap = function( data ){
 		
 		for(var i=0 ; i<data.map.points.length ; i++){
 			
@@ -42,6 +42,26 @@ function Map(){
 	
 	
 	
+	
+	this.worseTime = function( UserArray , point ){
+		
+		var res = -1 ;
+		for( var i=0 ; i < UserArray.length ; i++ ){
+			
+			var t = UserArray[i].vector[ point.id ];
+			
+			if( res == -1 )res = t;
+			else res = Math.max(res , t)
+			
+		}
+		return res;
+		
+	}
+
+
+	
+	
+	
 	this.naiveBestAdresse = function( UserArray ){
 
 		var find = false ;
@@ -61,6 +81,74 @@ function Map(){
 			}
 
 		} );
+
+		return res;
+
+	}*/
+
+
+
+	this.computeV = function( User ){
+
+		var MAX_NB_STEP = 3 ;
+
+		var Vo = {};
+		var wd = this.worseDistance ;
+
+		this.db( { "type":"point" } ).each( function(node){
+
+			Vo[ node.id ] = GEO.computeDistance(node.data.lat , node.data.lng , User.lat , User.lng);
+			Vo[ node.id ] /= 1.2 ;
+
+		} );
+
+		for( var step = 1 ; step < MAX_NB_STEP ; step ++ ){
+
+			var V = JSON.parse(JSON.stringify(Vo));
+			this.db( { "type":"edge" } ).each( function(edge){
+
+				if( (User.useTransports==true) ){
+
+					V[ edge.data.in ] = Math.min( Vo[ edge.data.in ] , Vo[ edge.data.out ] + edge.data.time.t );
+
+				}
+				else if(  edge.data.time.type != 'transport' ){
+
+					V[ edge.data.in ] = Math.min( Vo[ edge.data.in ] , Vo[ edge.data.out ] + edge.data.time.t );
+
+				}
+
+			} );
+			Vo = JSON.parse(JSON.stringify(V));
+
+		}
+
+		return Vo;
+
+	}
+
+
+	this.chooseBest = function( UserArray ){
+
+		var find = false ;
+		var bestValue = -1 ;
+		var res ;
+		var wt = this.worseTime ;
+		this.db( { "type":"point" } ).each( function(node){
+
+			var value = wt( UserArray , node.data ) ;
+
+			if( value < bestValue || find == false ){
+
+				find = true;
+				best = value;
+				res = node.data;
+
+			}
+
+		} );
+
+		return res;
 
 	}
 	
--- rechercheDePointDeRencontre/meetPointFinder.js
+++ rechercheDePointDeRencontre/meetPointFinder.js
@@ -1,11 +1,21 @@
-function MeetPointFinder () {
+
+function MeetPointFinder (){
 
 	this.maps = {} ;
 
-	this.findMeetPointFor( userArray , researchId , mapName ){
+	this.findMeetPointFor = function( userArray , researchId , mapName ){
 
 		return maps[ mapName ].naiveBestAdresse( userArray );
 
 	}
 
 }
+/*
+var fs = require('fs');
+var vm = require('vm');
+var includeInThisContext = function(path) {
+    var code = fs.readFileSync(path);
+    vm.runInThisContext(code, path);
+}.bind(this);
+includeInThisContext(__dirname+"/models/car.js");
+*/
